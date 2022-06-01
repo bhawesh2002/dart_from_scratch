d440ee7486ddb38bd25df8c62886f4891f989eaa
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-01 08:09:18 +0530

    concatenate two raw strings

--- basics/basics.dart
+++ basics/basics.dart
@@ -1,3 +1,6 @@
 void main(List<String> args) {
   print('Hello, world!'); //use of print funct to print raw strings
+  print("Hi I am Bhavesh" +
+      " I am from India"); //use of print funct to print raw strings
+  //"+" sign is used to concatenate two strings
 }
