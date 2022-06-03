dc0db63e09d2bd96b83bf1f22a24162f4f0c1043
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-03 20:31:39 +0530

    some changes

--- basics/basics.dart
+++ basics/basics.dart
@@ -1,11 +1,11 @@
 void main(List<String> args) {
   print('Hello, world!'); //use of print funct to print raw strings
-  print("Hi I am Bhavesh" +
+  print("Hi I am Bhavesh " +
       "and" +
       " I am from India"); //use of print funct to print raw strings
   //"+" sign is used to concatenate two strings
   print(
-      "\tCurrently I am studying in Pune"); //use of escape sequence to to add a tab in front the string
+      "\nCurrently I am studying in Pune"); //use of escape sequence to to add a tab in front the string
   print(
       "\nI am from Bhandara"); //use of escape sequence to to add a new line in front the string
 }
