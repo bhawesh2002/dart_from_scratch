d57bd1daa44e93f44efb728acf743f5a04d7bd7e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-06 08:28:33 +0530

    document the code

--- basics/data_types.dart
+++ basics/data_types.dart
@@ -1,11 +1,11 @@
 void main(List<String> args) {
   //numeric data types
-  int a = 10;
-  double b = 10.5;
-  num c = 11;
-  num d = 56.37;
-  var e = 12;
-  var f = 12.5;
+  int a = 10; //int is used to stroe integer values(no fractional part)
+  double b = 10.5; //double is used to store decimal values
+  num c = 11; //num is used to store both int and double values
+  num d = 56.37; //num is used to store both int and double values
+  var e = 12; //var is used to store any data type
+  var f = 12.5; //var is used to store any data type
   print(a);
   print(b);
   print(c);
