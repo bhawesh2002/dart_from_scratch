54adde2a02dd810c0dcb6049a26eba02e237e295
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-06 08:28:09 +0530

    different ways to declare numeric data_types

---
+++ basics/data_types.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  //numeric data types
+  int a = 10;
+  double b = 10.5;
+  num c = 11;
+  num d = 56.37;
+  var e = 12;
+  var f = 12.5;
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(e);
+  print(f);
+}
