7c0e1f90e026d0f483084476c16251a2a29360d2
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-08-14 16:05:02 +0530

    use for loop

--- basics/basic_functions.dart
+++ basics/basic_functions.dart
@@ -1,6 +1,9 @@
 void main(){
   print("Lets Greet you");
   List<String> names = ["Bhavesh", "Raj", "Rajesh", "Rajeshwari"];
+  for(String name in names){
+    print("Hello $name");
+  }
 }
 
 void greet(String name){
